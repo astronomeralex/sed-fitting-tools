c62b5c9248b6061ab5c727f4b791e9179f91ffec
Alex Hagen <mr.alex.hagen@gmail.com>
2013-12-06 17:23:44 -0500

    added more logging and removed silent variable

--- sedtools.py
+++ sedtools.py
@@ -17,10 +17,10 @@ class Flux(object):
     """
     this flux class will contain a single photometric measurement
     """
-    def __init__(self,flux,err,bandpassfile,silent=False):
+    def __init__(self,flux,err,bandpassfile):
         self.flux = flux
         self.err = err
-        self.filter = Filter(bandpassfile,silent)
+        self.filter = Filter(bandpassfile)
     
 ################################################################################
 
@@ -29,7 +29,7 @@ class Filter(object):
     the class contains information on the filter, including the name,
     location of the file describing the transmission, and the central wavelength
     """
-    def __init__(self,bandpassfile,silent = False):
+    def __init__(self,bandpassfile):
         self.transfile = bandpassfile #the location of the transmission file
         #now check and see if the transmission file exists
         if os.path.exists(self.transfile):
@@ -57,8 +57,7 @@ class Filter(object):
             self.long10 = waves[np.where(trans > 0.1)[0][-1]]
             
         else:
-            if not silent:
-                logger.warning("Can't find transmission file at " + self.transfile)
+            logger.error("Can't find transmission file at " + self.transfile)
                 
     def 
 
@@ -74,21 +73,27 @@ class Galaxy(object):
         """
         #sanity checking the inputs
         if len(fluxlist) == 0:
-            logger.critical("Input fluxlist is length zero")
+            raise ValueError("Input fluxlist is length zero")
         elif False in [isinstance(Flux,i) for i in fluxlist]:
             raise TypeError("Input fluxlist should only contain instance of Flux")
             
-        if type(name) != (str or int):
-            logger.critical("Galaxy name is not a string or integer -- output filesnames could have problems!")
+        if type(name) != str:
+            try:
+                nametype = type(str)
+                name = str(name)
+                logger.info("Galaxy name cast from " + str(nametype) + "to string: name is " + name)
+            except ValueError:
+                raise ValueError("Galaxy name input of type " + str(nametype) + " cannot be cast to string")
             
         if type(redshift) != float:
             try: 
                 inputztype = type(redshift)
                 redshift = float(redshift)
-                logger.info("Cast redshift from input " + str(inputztype) + "to float")
+                logger.info("Redshift cast from input " + str(inputztype) + "to float: z = " + str(redshift))
             except ValueError:
                 raise ValueError("Galaxy redshift input of type " + str(inputztype) + " cannot be cast to float!")
-        
+        if redshift < 0:
+            raise ValueError("Redshift is less than zero: z = " + str(redshift))
         
         self.name = name
         self.z = redshift
@@ -104,11 +109,18 @@ class Galaxy(object):
                 self.sedfluxlist.append(i)
                 
         if len(sedfluxlist) == 0:
-            logger.critical("")
+            logger.critical("Fluxlist for SED fitting is length zero")
+        else:
+            #sort fluxlist by central filter wavelength
+            self.sedfluxlist.sort(key = lambda x: x.filter.central)
         
-        
+        self.numphot = len(self.fluxlist)
+        self.numsedphot = len(self.sedfluxlist)
+            
     def __str__(self):
-        return "Galaxy: str(self.name) + " z=" + "%.3f" %self.z 
+        return "Galaxy: "+ str(self.name) + " z=" + "%.3f" %self.z
+        
+
 
 ################################################################################
 
